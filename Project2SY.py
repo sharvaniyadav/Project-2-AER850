3da037ad2f857f0095217fbd8a6baa4d6989963b
Sharvani Yadav <s1yadav@torontomu.ca>
2024-11-14 12:58:14 -0500

    Commit 1
    Cleaning up data defining paths & validation data variable definition.

--- Project2SY.py
+++ Project2SY.py
@@ -1,9 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov  1 12:29:23 2024
 
-@author: Sharvani Yadav
-"""
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+import matplotlib.pyplot as plt
 
-import numpy as np
-import pandas as pd
+# ------------------------------
+# Step 1: Data Processing
+# ------------------------------
+
+# Define image dimensions and batch size
+img_height = 500
+img_width = 500
+batch_size = 32
+
+
+# Define Directory Paths for Training and Validation Data
+
+train_directory = 'data/train'  # Relative path to train folder
+valid_directory = 'data/valid'  # Relative path to validation folder
+test_directory = 'data/test'    # Relative path to test folder
